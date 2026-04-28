0ab9eb4bddfcf58c457696f12b98f80d9596e83b
mayankkandpal4800-mynk <mayankkandpal4800@gmail.com>
2026-04-28 13:37:24 +0000

    prototype

--- 10_oops/Prototype.js
+++ 10_oops/Prototype.js
@@ -17,5 +17,50 @@ Object.prototype.mayank = function(){
     console.log(`mayank is present in all object`);
 }
 
+Array.prototype.heymayank = function(){
+    console.log(`Mayank says hello`);
+}
 // heroPower.mayank()
 myHeros.mayank()
+myHeros.heymayank()
+// heroPower.heymayank()
+
+
+
+// INHERITANCE 
+
+const User = {
+    name: "chai",
+    email: "lens@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false 
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment', 
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+// Teacher.__proto__ = User
+
+// modern syntax 
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUsername = "lensbymayank     "
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`${this.name}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"mayank".trueLength()
+"iceTea".trueLength()
